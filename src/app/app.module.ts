187aec1e97001dadccedb3fb751e75d339fb9dc3
R-V-F <100313927+R-V-F@users.noreply.github.com>
2022-07-09 17:07:48 -0300

    Add: styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GreetingsComponent } from './components/greetings/greetings.component';
@@ -12,7 +13,9 @@ import { GreetingsComponent } from './components/greetings/greetings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
