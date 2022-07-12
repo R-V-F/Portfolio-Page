f49e633c737d9c22c19caa39229beed0c537e7a5
R-V-F <100313927+R-V-F@users.noreply.github.com>
2022-07-12 09:27:16 -0300

    Add: P1 comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GreetingsComponent } from './components/greetings/greetings.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ProjectTemplateComponent } from './components/project-template/project-template.component';
+import { P1Component } from './components/p1/p1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GreetingsComponent,
-    NavComponent
+    NavComponent,
+    ProjectTemplateComponent,
+    P1Component
   ],
   imports: [
     BrowserModule,
